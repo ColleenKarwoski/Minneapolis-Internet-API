6d61e7237176fedfee4dded49a1f65b97c5355ea
dlee4912 <dlee4912@gmail.com>
2023-04-13 19:03:45 -0500

    modified structure

--- static/js/map.js
+++ static/js/map.js
@@ -1,5 +1,9 @@
 
+let queryUrl = 'http://127.0.0.1:5000/'
 
+d3.json(queryUrl).then(function (data) {
+    console.log(data)
+});
 
 let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
